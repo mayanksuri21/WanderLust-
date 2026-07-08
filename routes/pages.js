f15c1aa76ac1931a2c86b9e2b1f98becfe11e00d
Mayank Suri <mayanksurixb18@gmail.com>
2026-07-08 21:12:55 +0530

    style: overhaul navbar to full-width desktop layout and fix hero button contrast

--- routes/pages.js
+++ routes/pages.js
@@ -5,6 +5,10 @@ router.get("/about", (req, res) => {
   res.render("pages/about");
 });
 
+router.get("/features", (req, res) => {
+  res.render("pages/features");
+});
+
 router.get("/privacy", (req, res) => {
   res.render("pages/privacy");
 });
